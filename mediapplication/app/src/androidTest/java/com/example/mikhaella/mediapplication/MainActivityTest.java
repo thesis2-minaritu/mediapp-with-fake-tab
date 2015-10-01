601f96148e0d574d19a3b4e1bdc34d7d81887ca0
thesis2-minaritu <thesis2.minaritu@gmail.com>
2015-10-01 08:41:22 +0800

    with boxes
    with boxes for medicine info and hiding tabs

---
+++ mediapplication/app/src/androidTest/java/com/example/mikhaella/mediapplication/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.mikhaella.mediapplication;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Mikhaella on 9/22/2015.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnTabSelected() throws Exception {
+
+    }
+
+    public void testOnTabUnselected() throws Exception {
+
+    }
+
+    public void testOnTabReselected() throws Exception {
+
+    }
+}
---
+++ mediapplication/app/src/main/java/com/example/mikhaella/mediapplication/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.mikhaella.mediapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupToolbar();
+        setupRecyclerView();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        // Let's use a grid with 2 columns.
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        populateRecyclerView(recyclerView);
+
+        View tabBar = findViewById(R.id.fake_tab);
+        View coloredBackgroundView = findViewById(R.id.colored_background_view);
+        View toolbarContainer = findViewById(R.id.toolbar_container);
+        View toolbar = findViewById(R.id.toolbar);
+
+        recyclerView.setOnScrollListener(new ToolbarHidingOnScrollListener(toolbarContainer, toolbar, tabBar, coloredBackgroundView));
+    }
+
+
+    private void populateRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+
+            private final static int DUMMY_ITEM_COUNT = 5;
+
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+                View itemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+                return new TextHolder(itemView);
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+                // We are too lazy for this by now ;-)
+            }
+
+            @Override
+            public int getItemCount() {
+                return DUMMY_ITEM_COUNT;
+            }
+
+
+            class TextHolder extends RecyclerView.ViewHolder {
+
+                public TextHolder(View itemView) {
+                    super(itemView);
+                }
+            }
+        });
+    }
+}
